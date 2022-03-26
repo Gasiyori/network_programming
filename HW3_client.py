cc4f7d30353e16d1c7b33c56a6bb705d4d75333f
Giyeong Hong <ghdrldud99@gmail.com>
2022-03-27 04:31:51 +0900

    network programming HW3, 2022-03-27

---
+++ HW3_client.py
@@ -0,0 +1,22 @@
+# TCP 통신을 통한 서버-클라이언트간 계산기 프로그램
+# 클라이언트
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+addr = ('localhost', 9000)
+sock.connect(addr)
+
+while True:
+    msg = input("Enter a formula for calculation with space : ")
+
+    if msg == 'q': # q 입력시 종료
+        break
+    else: # 그 외(수식 등)의 경우
+        try:
+            sock.send(msg.encode()) # 인코딩해서 전송
+        except: # 오류 발생 예외처리
+            print("Error!")
+            break
+    
+    print(sock.recv(1024).decode()) # 디코딩해서 결과 출력
---
+++ HW3_server.py
@@ -0,0 +1,34 @@
+# TCP 통신을 통한 서버-클라이언트간 계산기 프로그램
+# 서버
+
+from shutil import which
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 9000)) # 바인딩
+s.listen(2) # 동시 2개까지 소켓 연결 가능
+client, addr = s.accept()
+
+while True:
+    msg = client.recv(1024).decode()
+    
+    num1, op, num2 = msg.split()
+
+    num1, num2 = int(num1), int(num2)
+
+    if op == '+':
+        result = num1 + num2
+        result = str(result)
+    elif op == '-':
+        result = num1 - num2
+        result = str(result)
+    elif op == '*':
+        result = num1 * num2
+        result = str(result)
+    elif op == '/': # round 활용하여 소수점 1자리까지 반올림처리
+        result = round((num1 / num2), 1)
+        result = str(result)
+    else:
+        result = "Operand is wrong!"
+    
+    client.send(result.encode())
